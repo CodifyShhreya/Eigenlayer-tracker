49523733ab6f35652396043a74e18d852519c680
Shhreya c seth <shreya.agent@gmail.com>
2026-09-15 06:00:15 +0530

    Create site.ts

---
+++ config /site.ts
@@ -0,0 +1,38 @@
+ export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "EigenLayer AVS Node Performance Tracker",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "PufferData",
+      href: "/pufferdata"
+    },
+    {
+      title: "Operator",
+      href: "/operator"
+    },
+    {
+      title: "AVS",
+      href: "/avs"
+    },
+    {
+      title: "Search Operator of an AVS",
+      href: "/searchoperatorofavs"
+    },
+    {
+      title: "Track with MoveSDK",
+      href: "/fetchmovedata"
+    }
+  ],
+  links: {
+    twitter: "https://twitter.com/soldegenbuilder7",
+    github: "https://github.com/degenbuilder7/eigenlayer-node-performance-tracker",
+    docs: "https://ui.shadcn.com",
+  },
+}
